4a35e51f240c09d7c072f96952a89d4f0a85df36
Matej87 <matej.zupcan@gmail.com>
2017-03-25 10:11:33 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Definiowanie wlasciwosci
+
+var person = {
+  name: "Matej"
+};
+
+person.age = 30;
+
+// [[Put]] wewnetryna metoda JS do tworyenia nowych wlasciwosci
+
+// [[Set]] nadpisuje jakas wartosc ktora juz istnieje
+
+person.name = "Karol";
+console.log(person.name); //'Bartek'
---
+++ script2.js
@@ -0,0 +1,12 @@
+// Wykrywanie wlasciwosci
+
+if(persona.age) {
+  //
+}
+
+var car = {
+  manufacturer: "Ford"
+};
+
+console.log('manufacturer in car'); //true
+//Warunek if zwroci false gdy bedzie rowne null, undefined, 0, false, Nan, ''
