4bd3e965cb64d5675c8b6c315a8c2517a5728e3d
Matej87 <matej.zupcan@gmail.com>
2017-03-26 16:47:43 +0200

    add nodemon as dev dep; add simple form; create post on subm

--- Server.js
+++ Server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const app = express();
+const port = 3000;
 
-express;
+app.listen(3000, () => {
+   console.log(`Server is running on port ${port}`);
+});
 
-console.log ("Server is running");
+app.get('/', (req, res) => {
+   console.log(__dirname);
+   res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/titles', (req, res) => {
+   console.log('Wait for titles');
+});
